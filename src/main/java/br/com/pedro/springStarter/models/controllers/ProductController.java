1a9c33f2c20b092aff6a7e5ae194c99f73850111
Pedro Henrique Pereira <pedropereira2312@gmail.com>
2023-07-24 16:55:24 -0300

    update[controllers/ProductController]: dependency injection

--- src/main/java/br/com/pedro/springStarter/models/controllers/ProductController.java
+++ src/main/java/br/com/pedro/springStarter/models/controllers/ProductController.java
@@ -1,19 +1,26 @@
 package br.com.pedro.springStarter.models.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.pedro.springStarter.models.entities.Product;
+import br.com.pedro.springStarter.models.repositories.ProductRepository;
 
 @RestController
 @RequestMapping("/api/produtos")
 public class ProductController {
 	
+	@Autowired
+	ProductRepository productRepository;
+	
 	@PostMapping("/novoProduto")
 	public Product newProduct(@RequestParam String name) {
-		return new Product(name);
+		Product newProduct = new Product(name);
+		productRepository.save(newProduct);
+		return newProduct;
 	}
 
 }
