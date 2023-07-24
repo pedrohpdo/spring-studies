d8794fc0d83ddc0baac7194c0e5e23226ebd5192
Pedro Henrique Pereira <pedropereira2312@gmail.com>
2023-07-24 16:20:30 -0300

    update: new project

---
+++ src/test/java/br/com/pedro/springStarter/SpringStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pedro.springStarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
