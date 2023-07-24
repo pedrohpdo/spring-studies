5e8711ef37d0b2ead524bcb8e0438611bb5043eb
Pedro Henrique Pereira <pedropereira2312@gmail.com>
2023-07-24 16:37:43 -0300

    update [models/entities]: new Product Class

--- src/main/java/br/com/pedro/springStarter/models/entities/Product.java
+++ src/main/java/br/com/pedro/springStarter/models/entities/Product.java
@@ -1,5 +1,43 @@
 package br.com.pedro.springStarter.models.entities;
 
-public class Product {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
+
+@Entity
+@Table(name = "products_table")
+public class Product {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false, length = 30)
+	private String name;
+	
+	public Product(String name) {
+		this.name = name;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
 }
