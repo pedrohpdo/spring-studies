b4ce6fd660218d645249ad119356b0db6d23d306
Pedro Henrique Pereira <pedropereira2312@gmail.com>
2023-07-25 17:58:15 -0300

    update[ProductController]: new deleteProductById()

--- src/main/java/br/com/pedro/springStarter/models/controllers/ProductController.java
+++ src/main/java/br/com/pedro/springStarter/models/controllers/ProductController.java
@@ -3,6 +3,7 @@ package br.com.pedro.springStarter.models.controllers;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -41,6 +42,12 @@ public class ProductController {
 	@PutMapping
 	public Product updateProduct(@Valid Product produto) {
 		return productRepository.save(produto);
+	}
+	
+	@DeleteMapping(path="/{id}")
+	public String deleteProductById(@PathVariable Long id) {
+		productRepository.deleteById(id);
+		return "sucess";
 	}
 	
 	
