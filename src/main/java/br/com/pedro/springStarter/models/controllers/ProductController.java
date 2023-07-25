9ad4a894bb1b85803ea9b5fad96f7743025d11b0
Pedro Henrique Pereira <pedropereira2312@gmail.com>
2023-07-25 19:53:42 -0300

    update[ProductController]: new getPages()

--- src/main/java/br/com/pedro/springStarter/models/controllers/ProductController.java
+++ src/main/java/br/com/pedro/springStarter/models/controllers/ProductController.java
@@ -3,6 +3,8 @@ package br.com.pedro.springStarter.models.controllers;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -37,6 +39,13 @@ public class ProductController {
 	@GetMapping(path="/{id}")
 	public @ResponseBody Optional<Product> getProductById(@PathVariable Long id) {
 		return productRepository.findById(id);
+	}
+	
+	@GetMapping(path="/page/{numberPage}")
+	public @ResponseBody Iterable<Product> getPages(@PathVariable int numberPage){
+		Pageable page = PageRequest.of(numberPage, 3);
+		
+		return productRepository.findAll(page);
 	}
 	
 	@PutMapping
