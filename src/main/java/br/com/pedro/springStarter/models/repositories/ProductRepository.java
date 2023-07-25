298b2a0a2ff29b2d4aa2d9bd74279983901c6dee
Pedro Henrique Pereira <pedropereira2312@gmail.com>
2023-07-25 18:20:51 -0300

    update[ProductRepository]: implements PagingAndSorting interface

--- src/main/java/br/com/pedro/springStarter/models/repositories/ProductRepository.java
+++ src/main/java/br/com/pedro/springStarter/models/repositories/ProductRepository.java
@@ -1,11 +1,12 @@
 package br.com.pedro.springStarter.models.repositories;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 import br.com.pedro.springStarter.models.entities.Product;
 
 @Repository
-public interface ProductRepository extends CrudRepository<Product, Long>{
+public interface ProductRepository extends PagingAndSortingRepository<Product, Long>, CrudRepository<Product, Long>{
 
 }
