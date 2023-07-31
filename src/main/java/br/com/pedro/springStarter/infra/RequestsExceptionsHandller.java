f6b4d2262fe658f3e9554bba393b096c221188f6
Pedro Henrique Pereira <pedropereira2312@gmail.com>
2023-07-31 11:12:33 -0300

    update: new Exception Treatement class - RequestExceptionHanddler

---
+++ src/main/java/br/com/pedro/springStarter/infra/RequestsExceptionsHandller.java
@@ -0,0 +1,8 @@
+package br.com.pedro.springStarter.infra;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class RequestsExceptionsHandller {
+	
+}
