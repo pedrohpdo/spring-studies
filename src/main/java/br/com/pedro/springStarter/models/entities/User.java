3c953c561848bd1815ac6c50b6abada0f9f1fcc2
Pedro Henrique Pereira <pedropereira2312@gmail.com>
2023-08-08 17:27:38 -0300

    update: implements new User.class

--- src/main/java/br/com/pedro/springStarter/models/entities/User.java
+++ src/main/java/br/com/pedro/springStarter/models/entities/User.java
@@ -1,14 +1,20 @@
 package br.com.pedro.springStarter.models.entities;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.GeneratedValue;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Table(name = "users")
@@ -18,40 +24,41 @@ import java.util.Collection;
 @NoArgsConstructor
 public class User implements UserDetails {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
     private String id;
     private String login;
     private String password;
-    private String role;
+    private UserRoles role;
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+
+        if (this.role == UserRoles.ADMIN) return List.of(new SimpleGrantedAuthority("ROLE_ADMIN"), new SimpleGrantedAuthority("ROLE_USER"));
+        else return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+
     }
 
     @Override
     public String getUsername() {
-        return null;
+        return this.login;
     }
 
     @Override
     public boolean isAccountNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isEnabled() {
-        return false;
+        return true;
     }
 }
---
+++ src/main/java/br/com/pedro/springStarter/models/entities/UserRoles.java
@@ -0,0 +1,16 @@
+package br.com.pedro.springStarter.models.entities;
+
+public enum UserRoles {
+    ADMIN("admin"),
+    USER("user");
+
+    private String role;
+
+    UserRoles(String role) {
+        this.role = role;
+    }
+
+    public String getRole(){
+        return role;
+    }
+}
