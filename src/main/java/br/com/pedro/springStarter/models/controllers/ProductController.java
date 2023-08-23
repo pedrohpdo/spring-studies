7da872e86014e2edb288ee11a813ae9b9ea0a5d8
Pedro Henrique Pereira <pedropereira2312@gmail.com>
2023-08-23 06:52:25 -0300

    update: organize imports

--- src/main/java/br/com/pedro/springStarter/models/controllers/ProductController.java
+++ src/main/java/br/com/pedro/springStarter/models/controllers/ProductController.java
@@ -1,5 +1,6 @@
 package br.com.pedro.springStarter.models.controllers;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,55 +21,55 @@ import org.springframework.web.bind.annotation.RestController;
 import br.com.pedro.springStarter.models.entities.ProductDTO;
 import br.com.pedro.springStarter.service.ProductService;
 import jakarta.validation.Valid;
+import org.springframework.web.util.UriComponentsBuilder;
 
 /**
  * Controller responsável por determinar as funções da api
- * 	
- * @author Pedro Henrique Pereira de Oliveira
  *
+ * @author Pedro Henrique Pereira de Oliveira
  */
 
 @RestController()
 @RequestMapping("/api/produtos")
 public class ProductController {
 
-	
-	@Autowired
-	private ProductService productService;
-		
-	@PostMapping
-	@ResponseStatus(code = HttpStatus.CREATED)
-	public @ResponseBody ResponseEntity<ProductDTO> create(@RequestBody @Valid ProductDTO requestedProduct) {
-		return ResponseEntity.ok(productService.create(requestedProduct));
-	}
-		
-	@GetMapping
-	public @ResponseBody ResponseEntity<List<ProductDTO>> get() {
-		return ResponseEntity.ok(productService.get());
-	}
-	
-	@GetMapping(path = "/{id}")
-	public @ResponseBody ResponseEntity<ProductDTO> get(@PathVariable Long id) {
-		return ResponseEntity.ok(productService.get(id));
-	}
-		
-	@GetMapping(path = "/page/{numberPage}")
-	public @ResponseBody ResponseEntity<Iterable<ProductDTO>> getByPage(@PathVariable int numberPage) {
 
-		return ResponseEntity.ok(productService.getByPage(numberPage));
-	}
-	
-	@PutMapping
-	@Transactional
-	public ResponseEntity<ProductDTO> update(@RequestBody @Valid ProductDTO data) {
-		return ResponseEntity.ok(productService.update(data));
-	}
-	
-	@DeleteMapping(path = "/{id}")
-	@Transactional
-	public ResponseEntity<Void> delete(@PathVariable Long id) {
-		productService.delete(id);
-		return ResponseEntity.noContent().build();
-	}
+    @Autowired
+    private ProductService productService;
+
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public @ResponseBody ResponseEntity<ProductDTO> create(@RequestBody @Valid ProductDTO requestedProduct) {
+        return ResponseEntity.ok().body(productService.create(requestedProduct));
+    }
+
+    @GetMapping
+    public @ResponseBody ResponseEntity<List<ProductDTO>> get() {
+        return ResponseEntity.ok(productService.get());
+    }
+
+    @GetMapping(path = "/{id}")
+    public @ResponseBody ResponseEntity<ProductDTO> get(@PathVariable Long id) {
+        return ResponseEntity.ok(productService.get(id));
+    }
+
+    @GetMapping(path = "/page/{numberPage}")
+    public @ResponseBody ResponseEntity<List<ProductDTO>> getByPage(@PathVariable int numberPage) {
+
+        return ResponseEntity.ok(productService.getByPage(numberPage));
+    }
+
+    @PutMapping
+    @Transactional
+    public ResponseEntity<ProductDTO> update(@RequestBody @Valid ProductDTO data) {
+        return ResponseEntity.ok(productService.update(data));
+    }
+
+    @DeleteMapping(path = "/{id}")
+    @Transactional
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        productService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 
 }
