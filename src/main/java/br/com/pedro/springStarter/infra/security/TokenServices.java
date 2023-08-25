5f9d028de7d47286c01a2995982cbc0bfdb3b7db
Pedro Henrique Pereira <pedropereira2312@gmail.com>
2023-08-24 23:23:44 -0300

    feat: implements token create/get service

---
+++ src/main/java/br/com/pedro/springStarter/infra/security/TokenServices.java
@@ -0,0 +1,56 @@
+package br.com.pedro.springStarter.infra.security;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+
+import br.com.pedro.springStarter.models.entities.User;
+
+@Service
+public class TokenServices {
+
+	@Value("api.security.token.secret")
+	private String secret;
+
+	private Instant instant = LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+
+	public String generateToken(User user) {
+		try {
+			Algorithm algorithm = Algorithm.HMAC256("123456");
+			return JWT
+					.create()
+					.withIssuer("API Voll.med")
+					.withSubject(user.getEmail())
+					.withClaim("password", user.getPassword())
+					.withExpiresAt(this.instant).sign(algorithm);
+
+		} catch (JWTCreationException e) {
+
+			throw new RuntimeException("error: " + e.getMessage());
+		}
+	}
+
+	public String getUser(String token) {
+		try {
+			Algorithm alg = Algorithm.HMAC256("123456");
+			return JWT
+					.require(alg)
+					.withIssuer("API Voll.med")
+					.build()
+					.verify(token)
+					.getSubject();
+
+		} catch (Exception e) {
+			throw new JWTVerificationException("TOKEN EXPIRED!: " + e.getMessage());
+		}
+	}
+
+}
